35ad5d363b9311a14769cecfce3b87be14ffbf96
JamesSwatton <jamessswatton@gmail.com>
2020-01-25 16:10:04 +0000

    add ship placement logic based on clear positions

--- main.js
+++ main.js
@@ -28,12 +28,26 @@ function horizonatalOrVertical() {
 };
 
 function shipCanBePlaced(x, y, ship, direction) {
+    let possiblePlacementPos = [];
+
     if (direction === 'horizontal') {
+        // gather possible placment positions 
+        for (let i = x; i < (x + ship.length); i++) {
+            possiblePlacementPos.push(board[y][i]);
+        };
+        console.log(possiblePlacementPos);
         if (board[y][x + (ship.length - 1)] === undefined) {
             return false;
-    }
+        } else if (possiblePlacementPos.every(pos => pos == '~')) {
+            return true;
+        }
         return true;
+
     } else if (direction === 'vertical') {
+        // gather possible placment positions 
+        for (let i = y; i < (y + ship.length); i++) {
+            possiblePlacementPos.push(board[i][x]);
+        };
         if (board[y + (ship.length - 1)][x] === undefined) {
             return false;
         }
@@ -45,9 +59,9 @@ const shipStartPositions = [];
 
 // collects all starting positions that a ship can be placed at
 function calcStartPositions() {
-    
+     
 };
 
 createBoard();
 
-// console.log(shipCanBePlaced(4, 1, ships[0], 'horizontal'));
+console.log(shipCanBePlaced(3, 1, ships[0], 'horizontal'));
