5d208993f7a0372e5d43b8782c223166b8bad19d
JamesSwatton <jamessswatton@gmail.com>
2020-01-25 13:10:49 +0000

    add createBoard function

--- main.js
+++ main.js
@@ -6,8 +6,18 @@ const horVert = [
 ]
 
 function horizonatalOrVertical() {
-    const randIndex= Math.floor(Math.random() * Math.floor(2));
+    const randIndex = Math.floor(Math.random() * Math.floor(2));
     return horVert[randIndex];
 }
 
-console.log(horizonatalOrVertical());
+function createBoard() {
+    for (let i = 0; i < 8; i++) {
+        board[i] = [];
+        for (let j = 0; j < 8; j++) {
+            board[i][j] = '~';
+        }
+    }
+}
+
+createBoard();
+console.log(board);
